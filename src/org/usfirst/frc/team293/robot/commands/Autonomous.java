07b61a07b426736e9345bfb033c2ac18f2cd2fc8
FRC Team 293 Hopewell, NJ <293.First@gmail.com>
2016-03-12 23:53:41 -0500

    Update Autonomous.java

--- src/org/usfirst/frc/team293/robot/commands/Autonomous.java
+++ src/org/usfirst/frc/team293/robot/commands/Autonomous.java
@@ -9,6 +9,7 @@ public class Autonomous extends CommandGroup {
     
     public  Autonomous() {//what is this going to do?  We have to work on IMU a lot.  Not sure how PID subsystem works
     	addSequential(new StartDriveStraight(10.0));
+    	add Sequential(new aimAndShoot());
     	//addSequential(new Aim());
     	//addSequential(new ShootHighGoal());
         // Add Commands here:
